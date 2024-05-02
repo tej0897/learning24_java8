235a71084d9ab990239383fa85deea315cb5b743
tej0897 <tej0897@gmail.com>
2024-05-02 11:53:31 +0530

    Lambdas commit

--- src/main/java/org/learning24/java8/ImperativeVsDeclarative/ImperativeVsDeclarativeExample1.java
+++ src/main/java/org/learning24/java8/ImperativeVsDeclarative/ImperativeVsDeclarativeExample1.java
@@ -1,2 +1,38 @@
-package org.learning24.java8.ImperativeVsDeclarative;public class ImperativeVsDeclarativeExample1 {
+package org.learning24.java8.ImperativeVsDeclarative;
+
+import java.util.stream.IntStream;
+
+/**
+ * Objective: summation of first 100 numbers
+ */
+public class ImperativeVsDeclarativeExample1 {
+    public static void main(String[] args) {
+
+        /**
+         * Imperative - how style of programming
+         */
+
+        int sum = 0;
+        for(int i=0; i<=100; i++){
+            sum = sum+i;
+        }
+        System.out.println("Sum using Imperative Approach: "+sum);
+
+        /**
+         * Declarative - What style of programing
+         */
+
+        int sum1 = IntStream.rangeClosed(0, 100).sum();
+        System.out.println("Sum using Declarative Approach: "+sum1);
+
+    }
+
+
+
+
+
+
+
+
+
 }
